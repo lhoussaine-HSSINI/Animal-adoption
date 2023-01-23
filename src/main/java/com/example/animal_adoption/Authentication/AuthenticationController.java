70359b547b456fcc08332470844b8100297d0911
lhoussaine-HSSINI <bontop.2018@gmail.com>
2023-01-23 09:36:28 +0100

    endde

--- src/main/java/com/example/animal_adoption/Authentication/AuthenticationController.java
+++ src/main/java/com/example/animal_adoption/Authentication/AuthenticationController.java
@@ -7,12 +7,10 @@ import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200/")
 @RequestMapping("/api/faho/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
